d379bc5f5e431b7e5e47c91431a843d0516319db
Yashvivanza <yashvivanza01@gmail.com>
2026-06-28 11:48:27 +0530

    New changes

---
+++ Opertors(Practice).dart
@@ -0,0 +1,38 @@
+// [1] even & odd no.
+/*void main()
+{
+    int a = 10;
+    if(a % 2 == 0)
+    {
+        print("Even");
+    }
+    else
+    {
+        print("Odd");
+    }
+}*/
+
+
+// [2] Find largest of two nos
+/*void main()
+{
+    int a = 10,b = 20;
+    print(a>b ? a : b);
+}*/
+
+
+// [3] Eligibilty for voting
+// [4] Positive and negative no
+
+// [5] Swap Two Numbers Without Third Variable
+/*void main() {
+  int a = 10;
+  int b = 20;
+
+  a = a + b;
+  b = a - b;
+  a = a - b;
+
+  print("a = $a");
+  print("b = $b");
+}*/
