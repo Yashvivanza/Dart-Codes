96853b5e9d691c3d5d487eec914a87f303129b1a
Yashvivanza <yashvivanza01@gmail.com>
2026-06-28 16:47:55 +0530

    List Practice

---
+++ List (Practice).dart
@@ -0,0 +1,56 @@
+// [1] Print all the elements of list
+/*void main()
+{
+  List<int> numbers = [10,20,30,40];
+  for(int i=0; i<numbers.length;i++)
+  {
+    print(numbers[i]);
+  }
+}*/
+
+
+// [2]Find sum of list elements
+/* void main()
+{
+  List<int> numbers = [10,20,30,40];
+  int sum =0;
+  for(var n in numbers)
+  {
+    sum += n;
+  }
+  print(sum);
+}*/
+
+
+// [3] List Methods
+// add() , addAll(), insert() , remove() , removeAt() , removeLast() 
+
+// [4] Sorting Questions
+// numbers.sort(); , numbers.reversed();
+
+
+//Take 5 Numbers from User
+/*import 'dart:io';
+
+void main() {
+  List<int> numbers = [];
+
+  for (int i = 1; i <= 5; i++) {
+    print("Enter Number:");
+    numbers.add(int.parse(stdin.readLineSync()!));
+  }
+
+  print(numbers);
+}*/
+
+// Sum
+/*import 'dart:io';
+
+void main(){
+List <int> numbers = [1,2,3,4];
+int sum = 0;
+for(var n in numbers){
+  sum += n;
+}
+print("SUM: $sum");
+}*/
